ada4e5f87ab069d5c2f2cf4b4020d4efe131ff0f
jonataswalker <jonataswalker@gmail.com>
2016-03-30 20:07:55 -0300

    Change jshint to eslint

--- src/html.js
+++ src/html.js
@@ -1,66 +1,69 @@
-(function(win, doc){
-  ContextMenu.Html = function(menu){
-    this.options = menu.options;
-    this.container = this.createContainer();
-  };
-  ContextMenu.Html.prototype = {
-    createContainer: function() {
-      var container = doc.createElement('ul');
-      container.className = 'ol-contextmenu ol-unselectable hidden';
-      container.style.width = parseInt(this.options.width, 10) + 'px';
-      return container;
-    },
-    createMenu: function() {
-      var options = this.options, items = [];
 
-      if('items' in options){
-        items = (options.default_items) ?
-          options.items.concat(ContextMenu.defaultItems) : options.items;
-      } else if(options.default_items){
-        items = ContextMenu.defaultItems;
-      }
-      
-      //no item
-      if(items.length === 0) return false;
+/**
+ * @constructor
+ */
+CM.Html = function(menu){
+  this.options = menu.options;
+  this.container = this.createContainer();
+};
 
-      // create entries
-      items.forEach(this.addMenuEntry, this);
-    },
-    addMenuEntry: function(item, index) {
-      var classname, style = '', html = '';
-      
-      //separator
-      if(typeof item === 'string'){
-        if(item.trim() == '-'){
-          html = '<li class="ol-menu-sep"><hr></li>';
-        }
-      } else {
-        if(item.icon){
-          item.classname += ' ol-contextmenu-icon';
-          style = ' style="background-image:url(' + item.icon + ')"';
-        }
-        
-        classname = item.classname ? ' class="' + item.classname + '"' : '';
-        html = '<li id="index' + index + '"' + style + classname + '>' +
-          item.text + '</li>';
-      }
+CM.Html.prototype = {
+  createContainer: function() {
+    var container = document.createElement('ul');
+    container.className = 'ol-contextmenu ol-unselectable hidden';
+    container.style.width = parseInt(this.options.width, 10) + 'px';
+    return container;
+  },
+  createMenu: function() {
+    var options = this.options, items = [];
 
-      var frag = utils.createFragment(html);
-      // http://stackoverflow.com/a/13347298/4640499
-      var child = [].slice.call(frag.childNodes, 0)[0];
-      this.container.appendChild(frag);
-
-      ContextMenu.items[index] = {
-        id: index,
-        callback: item.callback,
-        data: item.data || null
-      };
-      
-      // publish to add listener
-      events.publish(ContextMenu.Constants.eventType.ADD_MENU_ENTRY, {
-        index: index,
-        element: child
-      });
+    if('items' in options){
+      items = (options.default_items) ?
+        options.items.concat(CM.defaultItems) : options.items;
+    } else if(options.default_items){
+      items = CM.defaultItems;
     }
-  };
-})(win, doc);
+    
+    //no item
+    if(items.length === 0) return false;
+
+    // create entries
+    items.forEach(this.addMenuEntry, this);
+  },
+  addMenuEntry: function(item, index) {
+    var classname, style = '', html = '';
+    
+    //separator
+    if(typeof item === 'string'){
+      if(item.trim() == '-'){
+        html = '<li class="ol-menu-sep"><hr></li>';
+      }
+    } else {
+      if(item.icon){
+        item.classname += ' ol-contextmenu-icon';
+        style = ' style="background-image:url(' + item.icon + ')"';
+      }
+      
+      classname = item.classname ? ' class="' + item.classname + '"' : '';
+      html = '<li id="index' + index + '"' + style + classname + '>' +
+        item.text + '</li>';
+    }
+
+    var frag = utils.createFragment(html);
+    // http://stackoverflow.com/a/13347298/4640499
+    var child = [].slice.call(frag.childNodes, 0)[0];
+    this.container.appendChild(frag);
+
+    CM.items[index] = {
+      id: index,
+      callback: item.callback,
+      data: item.data || null
+    };
+    
+    // publish to add listener
+    events.publish(CM.Constants.eventType.ADD_MENU_ENTRY, {
+      index: index,
+      element: child
+    });
+  }
+};
