394a92c0464c9e9a52a2e3126135a3b0c266184d
jonataswalker <jonataswalker@gmail.com>
2016-03-31 22:43:20 -0300

    Add Submenu

--- src/html.js
+++ src/html.js
@@ -1,26 +1,25 @@
-
 /**
  * @constructor
  */
-CM.Html = function(menu){
-  this.options = menu.options;
-  this.container = this.createContainer();
+CM.Html = function(){
+  CM.container = this.createContainer();
 };
 
 CM.Html.prototype = {
   createContainer: function() {
+    var classes = CM.Constants.css;
     var container = document.createElement('ul');
-    container.className = 'ol-contextmenu ol-unselectable hidden';
-    container.style.width = parseInt(this.options.width, 10) + 'px';
+    container.className = classes.container +' '+ classes.hidden;
+    container.style.width = parseInt(CM.options.width, 10) + 'px';
     return container;
   },
   createMenu: function() {
-    var options = this.options, items = [];
+    var options = CM.options, items = [];
 
-    if('items' in options){
+    if ('items' in options) {
       items = (options.default_items) ?
         options.items.concat(CM.defaultItems) : options.items;
-    } else if(options.default_items){
+    } else if (options.default_items) {
       items = CM.defaultItems;
     }
     
@@ -30,40 +29,77 @@ CM.Html.prototype = {
     // create entries
     items.forEach(this.addMenuEntry, this);
   },
-  addMenuEntry: function(item, index) {
-    var classname, style = '', html = '';
+  addMenuEntry: function(item) {
+    var classes = CM.Constants.css;
+    var this_ = this;
+    var index = CM.$internal.getNextItemIndex();
     
-    //separator
-    if(typeof item === 'string'){
-      if(item.trim() == '-'){
-        html = '<li class="ol-menu-sep"><hr></li>';
-      }
-    } else {
-      if(item.icon){
-        item.classname += ' ol-contextmenu-icon';
-        style = ' style="background-image:url(' + item.icon + ')"';
+    if (item.items && Array.isArray(item.items)) {
+      // submenu - only a second level
+      item.classname = item.classname || '';
+      if (!utils.test(classes.submenu, item.classname)) {
+        item.classname += ' ' + classes.submenu;
       }
       
-      classname = item.classname ? ' class="' + item.classname + '"' : '';
-      html = '<li id="index' + index + '"' + style + classname + '>' +
-        item.text + '</li>';
+      var li = this.generateHtmlAndPublish(CM.container, item, index);
+      var ul = document.createElement('ul');
+      
+      ul.className = classes.container;
+      ul.style.left = CM.submenu.last_left || CM.submenu.left;
+      ul.style.width = CM.options.width + 'px';
+      li.appendChild(ul);
+      
+      item.items.forEach(function(each) {
+        this_.generateHtmlAndPublish(ul, each, CM.$internal.getNextItemIndex(), true);
+      });
+    } else {
+      this.generateHtmlAndPublish(CM.container, item, index);
     }
-
-    var frag = utils.createFragment(html);
-    // http://stackoverflow.com/a/13347298/4640499
-    var child = [].slice.call(frag.childNodes, 0)[0];
-    this.container.appendChild(frag);
-
-    CM.items[index] = {
-      id: index,
+  },
+  generateHtmlAndPublish: function (parent, item, i, submenu) {
+    var html, frag, element, separator = false;
+    var classes = CM.Constants.css;
+    
+    // separator
+    if (typeof item === 'string' && item.trim() == '-') {
+      html = '<li class="'+ classes.separator +'"><hr></li>';
+      frag = utils.createFragment(html);
+      // http://stackoverflow.com/a/13347298/4640499
+      element = [].slice.call(frag.childNodes, 0)[0];
+      parent.appendChild(frag);
+      // to exclude from lineHeight calculation
+      separator = true;
+    } else {
+      item.classname = item.classname || '';
+      html = '<span>'+ item.text +'</span>';
+      frag = utils.createFragment(html);
+      element = document.createElement('li');
+      
+      if(item.icon){
+        item.classname += ' ' + classes.icon;
+        element.setAttribute('style', 'background-image:url('+ item.icon +')');
+      }
+      
+      element.id = 'index' + i;
+      element.className = item.classname;
+      element.appendChild(frag);
+      parent.appendChild(element);
+    }
+    
+    CM.items[i] = {
+      id: i,
+      submenu: submenu || 0,
+      separator: separator,
       callback: item.callback,
       data: item.data || null
     };
     
     // publish to add listener
-    events.publish(CM.Constants.eventType.ADD_MENU_ENTRY, {
-      index: index,
-      element: child
+    events.publish(CM.EventType.ADD_MENU_ENTRY, {
+      index: i,
+      element: element
     });
+    
+    return element;
   }
 };
--- src/internal.js
+++ src/internal.js
@@ -2,12 +2,16 @@
 /**
  * @constructor
  */
-CM.Internal = function(menu){
+CM.Internal = function(){
   this.map = undefined;
-  this.contextmenu = menu;
-  this.container = menu.container;
-  this.$html = menu.$html;
   this.coordinate_clicked = undefined;
+  this.pixel_clicked = undefined;
+  this.counter = 0;
+  this.lineHeight = 0;
+  CM.submenu = {
+    left: CM.options.width - 15 + 'px',
+    last_left: '' // string + px
+  };
 };
 
 CM.Internal.prototype = {
@@ -16,45 +20,89 @@ CM.Internal.prototype = {
     // subscribe
     this.setListeners();
     // publish
-    this.$html.createMenu();
+    CM.$html.createMenu();
+    this.lineHeight = CM.container.offsetHeight / this.getItemsLength();
+  },
+  getItemsLength: function() {
+    var count = 0;
+    Object.keys(CM.items).forEach(function(k){
+      if (CM.items[k].submenu || CM.items[k].separator) return;
+      count++;
+    });
+    return count;
+  },
+  getPixelClicked: function() {
+    return this.pixel_clicked;
   },
   getCoordinateClicked: function() {
     return this.coordinate_clicked;
   },
+  positionContainer: function(pixel) {
+    var classes = CM.Constants.css,
+        map_size = this.map.getSize(),
+        map_w = map_size[0],
+        map_h = map_size[1],
+        // how much (width) space left over
+        space_left_h = map_h - pixel[1],
+        // how much (height) space left over
+        space_left_w = map_w - pixel[0],
+        menu_size = {
+          w: CM.container.offsetWidth,
+          // a cheap way to recalculate container height
+          // since offsetHeight is like cached
+          h: Math.round(this.lineHeight * this.getItemsLength())
+        },
+        // submenus <ul>
+        uls = utils.find('li.' + classes.submenu + '>ul', CM.container, true);
+    
+    if (space_left_w >= menu_size.w) {
+      CM.container.style.right = 'auto';
+      CM.container.style.left = pixel[0] + 5 + 'px';
+    } else {
+      CM.container.style.left = 'auto';
+      CM.container.style.right = 15 + 'px';
+    }
+    // set top or bottom
+    if (space_left_h >= menu_size.h) {
+      CM.container.style.bottom = 'auto';
+      CM.container.style.top = pixel[1] - 10 + 'px';
+    } else {
+      CM.container.style.top = 'auto';
+      CM.container.style.bottom = 0 + 'px';
+    }
+    
+    utils.removeClass(CM.container, classes.hidden);
+    
+    if (uls.length) {
+      if (space_left_w < (menu_size.w * 2)) {
+        // no space (at right) for submenu
+        // position them at left
+        CM.submenu.last_left = '-' + menu_size.w + 'px';
+      } else {
+        CM.submenu.last_left = CM.submenu.left;
+      }
+      uls.forEach(function(ul){
+        ul.style.left = CM.submenu.last_left;
+      });
+    }
+  },
   openMenu: function(pixel, coordinate) {
-    var map_size = this.map.getSize(),
-        menu_size = [
-          this.container.offsetWidth,
-          this.container.offsetHeight
-        ],
-        map_width     = map_size[0],
-        map_height    = map_size[1],
-        menu_width    = menu_size[0],
-        menu_height   = menu_size[1],
-        height_left   = map_height - pixel[1],
-        width_left    = map_width - pixel[0],
-        top = (height_left >= menu_height) ?
-          pixel[1] - 10 : pixel[1] - menu_height - 10,
-        left = (width_left >= menu_width) ?
-          pixel[0] + 5 : pixel[0] - menu_width - 5;
-
-    utils.removeClass(this.container, 'hidden');
-    this.container.style.left = left + 'px';
-    this.container.style.top = top + 'px';
-    this.contextmenu.dispatchEvent({
+    this.positionContainer(pixel);
+    
+    CM.$base.dispatchEvent({
       type: CM.EventType.OPEN,
       pixel: pixel,
       coordinate: coordinate,
     });
   },
   closeMenu: function(){
-    utils.addClass(this.container, 'hidden');
-    this.contextmenu.dispatchEvent({
+    utils.addClass(CM.container, CM.Constants.css.hidden);
+    CM.$base.dispatchEvent({
       type: CM.EventType.CLOSE
     });
   },
-  getNextItemIndex: function(){
-    return Object.keys(CM.items).length;
+  getNextItemIndex: function() {
+    return ++this.counter;
   },
   setListeners: function() {
     var this_ = this,
@@ -64,7 +112,8 @@ CM.Internal.prototype = {
           evt.stopPropagation();
           evt.preventDefault();
           this_.coordinate_clicked = map.getEventCoordinate(evt);
-          this_.openMenu(map.getEventPixel(evt), this_.coordinate_clicked);
+          this_.pixel_clicked = map.getEventPixel(evt);
+          this_.openMenu(this_.pixel_clicked, this_.coordinate_clicked);
           
           //one-time fire
           canvas.addEventListener('mousedown', {
@@ -77,7 +126,7 @@ CM.Internal.prototype = {
     canvas.addEventListener('contextmenu', menu, false);
     
     // subscribe to later menu entries
-    events.subscribe(CM.Constants.eventType.ADD_MENU_ENTRY,
+    events.subscribe(CM.EventType.ADD_MENU_ENTRY,
       function(obj) {
         this_.setItemListener(obj.element, obj.index);
       }
@@ -109,21 +158,29 @@ CM.EventType = {
   /**
     * Triggered when context menu is closed.
     */
-  CLOSE: 'close'
+  CLOSE: 'close',
+  /**
+    * Internal. Triggered when a menu entry is added.
+    */
+  ADD_MENU_ENTRY: 'add-menu-entry'
 };
 
 CM.items = {};
 
 CM.Constants = {
-  eventType: {
-    ADD_MENU_ENTRY: 'add-menu-entry'
+  css: {
+    container: 'ol-contextmenu ol-unselectable',
+    separator: 'ol-cm-separator',
+    submenu: 'ol-cm-submenu',
+    hidden: 'ol-cm-hidden',
+    icon: 'ol-cm-icon'
   }
 };
 
 CM.defaultItems = [
   {
     text: 'Zoom In',
-    classname: 'zoom-in ol-contextmenu-icon',
+    classname: 'zoom-in ' + CM.Constants.css.icon,
     callback: function(obj, map){
       var
         view = map.getView(),
@@ -144,7 +201,7 @@ CM.defaultItems = [
   },
   {
     text: 'Zoom Out',
-    classname: 'zoom-out ol-contextmenu-icon',
+    classname: 'zoom-out ' + CM.Constants.css.icon,
     callback: function(obj, map){
       var
         view = map.getView(),
