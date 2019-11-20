78517fc38cc28185ccdbe2c4fbec686be810c300
Jônatas Walker <jonatas.walker@autoavaliar.com.br>
2019-11-20 13:23:28 -0300

    Update Examples and some editor configs

--- examples/contextmenu.js
+++ examples/contextmenu.js
@@ -1,19 +1,19 @@
 /* global ol, ContextMenu */
 var view = new ol.View({ center: [0, 0], zoom: 4 }),
-    vectorLayer = new ol.layer.Vector({ source: new ol.source.Vector() }),
-    baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() }),
-    map = new ol.Map({
-      target: 'map',
-      view: view,
-      layers: [baseLayer, vectorLayer]
-    });
+  vectorLayer = new ol.layer.Vector({ source: new ol.source.Vector() }),
+  baseLayer = new ol.layer.Tile({ source: new ol.source.OSM() }),
+  map = new ol.Map({
+    target: 'map',
+    view: view,
+    layers: [baseLayer, vectorLayer],
+  });
 
 var contextmenu_items = [
   {
     text: 'Center map here',
     classname: 'bold',
     icon: 'img/center.png',
-    callback: center
+    callback: center,
   },
   {
     text: 'Some Actions',
@@ -22,44 +22,43 @@ var contextmenu_items = [
       {
         text: 'Center map here',
         icon: 'img/center.png',
-        callback: center
+        callback: center,
       },
       {
         text: 'Add a Marker',
         icon: 'img/pin_drop.png',
-        callback: marker
-      }
-    ]
+        callback: marker,
+      },
+    ],
   },
   {
     text: 'Add a Marker',
     icon: 'img/pin_drop.png',
-    callback: marker
+    callback: marker,
   },
-  '-' // this is a separator
+  '-', // this is a separator
 ];
 
 var removeMarkerItem = {
   text: 'Remove this Marker',
   classname: 'marker',
-  callback: removeMarker
+  callback: removeMarker,
 };
 
 var contextmenu = new ContextMenu({
   width: 180,
-  items: contextmenu_items
+  items: contextmenu_items,
 });
 map.addControl(contextmenu);
 
-
-contextmenu.on('open', function (evt) {
-  var feature = map.forEachFeatureAtPixel(evt.pixel, function (ft, l) {
+contextmenu.on('open', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel, function(ft, l) {
     return ft;
   });
   if (feature && feature.get('type') === 'removable') {
     contextmenu.clear();
     removeMarkerItem.data = {
-      marker: feature
+      marker: feature,
     };
     contextmenu.push(removeMarkerItem);
   } else {
@@ -69,8 +68,7 @@ contextmenu.on('open', function (evt) {
   }
 });
 
-map.on('pointermove', function (e) {
-
+map.on('pointermove', function(e) {
   var pixel = map.getEventPixel(e.originalEvent);
   var hit = map.hasFeatureAtPixel(pixel);
 
@@ -81,14 +79,16 @@ map.on('pointermove', function (e) {
 
 // from https://github.com/DmitryBaranovskiy/raphael
 function elastic(t) {
-  return Math.pow(2, -10 * t) * Math.sin((t - 0.075) * (2 * Math.PI) / 0.3) + 1;
+  return (
+    Math.pow(2, -10 * t) * Math.sin(((t - 0.075) * (2 * Math.PI)) / 0.3) + 1
+  );
 }
 
 function center(obj) {
   view.animate({
     duration: 700,
     easing: elastic,
-    center: obj.coordinate
+    center: obj.coordinate,
   });
 }
 
@@ -98,21 +98,21 @@ function removeMarker(obj) {
 
 function marker(obj) {
   var coord4326 = ol.proj.transform(obj.coordinate, 'EPSG:3857', 'EPSG:4326'),
-      template = 'Coordinate is ({x} | {y})',
-      iconStyle = new ol.style.Style({
-        image: new ol.style.Icon({ scale: .6, src: 'img/pin_drop.png' }),
-        text: new ol.style.Text({
-          offsetY: 25,
-          text: ol.coordinate.format(coord4326, template, 2),
-          font: '15px Open Sans,sans-serif',
-          fill: new ol.style.Fill({ color: '#111' }),
-          stroke: new ol.style.Stroke({ color: '#eee', width: 2 })
-        })
+    template = 'Coordinate is ({x} | {y})',
+    iconStyle = new ol.style.Style({
+      image: new ol.style.Icon({ scale: 0.6, src: 'img/pin_drop.png' }),
+      text: new ol.style.Text({
+        offsetY: 25,
+        text: ol.coordinate.format(coord4326, template, 2),
+        font: '15px Open Sans,sans-serif',
+        fill: new ol.style.Fill({ color: '#111' }),
+        stroke: new ol.style.Stroke({ color: '#eee', width: 2 }),
       }),
-      feature = new ol.Feature({
-        type: 'removable',
-        geometry: new ol.geom.Point(obj.coordinate)
-      });
+    }),
+    feature = new ol.Feature({
+      type: 'removable',
+      geometry: new ol.geom.Point(obj.coordinate),
+    });
 
   feature.setStyle(iconStyle);
   vectorLayer.getSource().addFeature(feature);
